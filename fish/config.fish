f4de6212a432186cb618e102e6d01ad9788041f6
Drew Zemke <drew.zemke@gmail.com>
2023-06-17 15:38:26 -0700

    fish: abbreviation `hx` for `helix` - on mac this abbrev. does nothing, since `hx` is already the right name

--- fish/config.fish
+++ fish/config.fish
@@ -5,8 +5,8 @@ fish_add_path -p /usr/local/sbin /usr/local/bin /usr/bin ~/.cargo/bin
 set -gx XDG_CONFIG_HOME ~/.config/
 
 # set editor and visual env variables
-set -gx EDITOR {{editor}}
-set -gx VISUAL {{editor}}
+set -gx EDITOR {{editor}};
+set -gx VISUAL {{editor}};
 
 # initialize zoxide
 zoxide init fish | source
@@ -16,6 +16,9 @@ abbr -a -- ls   'exa -1 --color=auto --icons --group-directories-first'
 abbr -a -- lst  'exa -1 --color=auto --icons --group-directories-first -T --git-ignore'
 abbr -a -- cat  bat
 abbr -a -- rst  'source ~/.config/fish/config.fish'
+
+# helix abbreviations
+abbr -a -- hx {{editor}};
 
 # git abbreviations
 abbr -a -- gs   'git status'
