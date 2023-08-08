11d4952898500532bbb298cfa40b1289119ce9d3
Drew Zemke <drew.zemke@gmail.com>
2023-08-07 21:53:27 -0700

    kmonad: add starter config

---
+++ kmonad/start_kmonad.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+device_name="usb-Apple__Inc_Apple_Keyboard-event-kbd"
+
+if [[ -e "/dev/input/by-id/${device_name}" ]]; then
+  /usr/bin/kmonad /home/drew/.config/kmonad/tutorial.kbd
+fi
+
+
+
