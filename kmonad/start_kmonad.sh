5ed615b8d9e016d9987a344de21dc388ff31a7e8
Drew Zemke <drew.zemke@gmail.com>
2023-08-18 18:37:35 -0700

    kmonad: `start_kmonad` script now only used on arch

--- kmonad/start_kmonad.sh
+++ kmonad/start_kmonad.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
+#
+# this script is only needed for arch
 device_name="usb-Apple__Inc_Apple_Keyboard-event-kbd"
 
 if [[ -e "/dev/input/by-id/${device_name}" ]]; then
-  /usr/bin/kmonad /home/drew/.config/kmonad/drew.kbd
+  /usr/bin/kmonad /home/drew/.config/kmonad/config.kbd
 fi
-
-
- 
