dab9bfc78a7281c5360d2de4beaaf58618c4daaa
Drew Zemke <drew.zemke@gmail.com>
2023-08-29 22:41:34 -0700

    kmonad: biggg overhaul to fit my layout onto a three-row setup, much slow typing in my future ._.

--- kmonad/start_kmonad.sh
+++ kmonad/start_kmonad.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #
 # this script is only needed for arch
-device_name="usb-Apple__Inc_Apple_Keyboard-event-kbd"
+apple_keyboard="usb-Apple__Inc_Apple_Keyboard-event-kbd"
+corne_keyboard="usb-foostan_Corne-event-kbd"
 
-if [[ -e "/dev/input/by-id/${device_name}" ]]; then
-  /usr/bin/kmonad /home/drew/.config/kmonad/config.kbd
+if [[ -e "/dev/input/by-id/${corne_keyboard}" ]]; then
+  /usr/local/bin/kmonad --input "device-file \"/dev/input/by-id/${corne_keyboard}\"" /home/drew/.config/kmonad/config.kbd
+elif [[ -e "/dev/input/by-id/${apple_keyboard}" ]]; then
+  /usr/local/bin/kmonad --input "device-file \"/dev/input/by-id/${apple_keyboard}\"" /home/drew/.config/kmonad/config.kbd 
 fi
