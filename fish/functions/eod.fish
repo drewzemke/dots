8394a778a41a68d9f0f2fb356417a974813e9dc5
Drew Zemke <drew.zemke@gmail.com>
2023-08-30 22:30:57 -0700

    fish: added start-of-day/end-of-day sync scripts'

---
+++ fish/functions/eod.fish
@@ -0,0 +1,29 @@
+#!/usr/bin/env fish
+#
+# end of day
+function eod
+	function log -a color bold message
+		echo (set_color (if $bold; echo "-o"; end) $color)$message
+		set_color normal
+	end
+
+	log magenta true "End of Day"
+
+	echo 
+	log green false "Pushing notes..."
+	git -C ~/notes add .
+	git -C ~/notes commit -m 'update '(date +"%a %b %d, %Y %H:%M")
+	git -C ~/notes push
+
+	echo
+	log cyan false "Pushing dots..."
+	if test (git -C ~/.dots status --porcelain | wc -l) -gt 0
+		log red true "Error: uncommited changes in dots. Please review and commit."
+		return 1
+	else
+		git -C ~/.dots push
+	end
+
+	echo
+	log magenta true "Mischief managed."
+end
---
+++ fish/functions/sod.fish
@@ -0,0 +1,22 @@
+#!/usr/bin/env fish
+#
+# start of day
+function sod 
+	function log -a color bold message
+		echo (set_color (if $bold; echo "-o"; end) $color)$message
+		set_color normal
+	end
+
+	log magenta true "Start of Day"
+
+	log green false "Pulling notes..."
+	git -C ~/notes pull
+
+	log cyan false "Pulling dots..."
+	git -C ~/.dots pull
+
+	log cyan false "Deploying dots..."
+	fish -c 'cd ~/.dots ; exec dotter'
+
+	log magenta true "Have a nice day!"
+end
