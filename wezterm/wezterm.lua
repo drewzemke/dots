97d62cc2ffb3a024b26a8d7d2bfd4c3220844c09
Drew Zemke <drew.zemke@gmail.com>
2023-10-08 13:00:03 -0700

    wezterm: config to more-or-less line up with alacritty

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,11 +14,55 @@ end
 -- first things first:
 config.color_scheme = 'Dracula'
 
+-- set font size and tracking
+config.font_size = 10.0
+config.cell_width = 1.0 -- this is the default; bump up to 1.1?
+
 -- hide the tab bar
 config.hide_tab_bar_if_only_one_tab = true
 
 -- hide the window title bar
-config.window_decorations = "RESIZE"
+config.window_decorations = 'RESIZE'
+
+-- keybindings
+config.keys = {
+
+  -- zellij pane movement rebindings
+  {
+    key = 'h',
+    mods = 'CMD|CTRL',
+    action = wezterm.action.SendKey { key = 'α' }
+  },
+  {
+    key = 'j',
+    mods = 'CMD|CTRL',
+    action = wezterm.action.SendKey { key = 'β' }
+  },
+  {
+    key = 'k',
+    mods = 'CMD|CTRL',
+    action = wezterm.action.SendKey { key = 'γ' }
+  },
+  {
+    key = 'l',
+    mods = 'CMD|CTRL',
+    action = wezterm.action.SendKey { key = 'δ' }
+  },
+  {
+    key = '\'',
+    mods = 'CMD|CTRL',
+    action = wezterm.action.SendKey { key = 'ϵ' }
+  },
+
+  -- TODO: turn off some things (at least for mac)
+}
+
+-- transparent bg 
+config.window_background_opacity = 0.92
+
+-- pump that framerate babyyy
+config.max_fps = 144
+
 
 -- and finally, return the configuration to wezterm
 return config
