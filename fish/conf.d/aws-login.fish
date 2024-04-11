4b2cddb43814033303e5af45c33b58edb4951639
Drew Zemke <drew.zemke@gmail.com>
2024-04-11 11:25:45 -0700

    fish: better aws-login alias

--- fish/conf.d/aws-login.fish
+++ fish/conf.d/aws-login.fish
@@ -1,2 +1,2 @@
 # aws-login abbreviation
-abbr -a -- aws-login 'aws-adfs login --profile default --adfs-ca-bundle ~/.aws/sectigo.cer && aws-adfs login --profile cross-account --adfs-ca-bundle ~/.aws/sectigo.cer'
+alias aws-login='aws-adfs login --profile default --adfs-ca-bundle ~/.aws/sectigo.cer --authfile ~/.aws-login --duo-factor "Duo Push" && aws-adfs login --profile cross-account --adfs-ca-bundle ~/.aws/sectigo.cer --authfile ~/.aws-login --duo-factor "Duo Push"'
