6c83ca4dfac3ae4b1c4aac9035492637802fead6
Drew Zemke <drew.zemke@gmail.com>
2024-07-13 17:57:12 -0700

    helix: remove highlights from dotter management

--- helix/runtime/queries/tsx/highlights.scm
+++
@@ -1,7 +0,0 @@
-(jsx_opening_element ["<" ">"] @jsx_brackets)
-(jsx_closing_element ["</" ">"] @jsx_brackets)
-(jsx_self_closing_element ["<" "/>"] @jsx_brackets)
-
-(jsx_attribute (property_identifier) @jsx_attribute)
-
-; inherits: _jsx,_typescript,ecma
