f14c4cdd9b78774dde6617919fb7413d85cbb730
Drew Zemke <drew.zemke@gmail.com>
2024-10-01 11:19:09 -0700

    nixos: updated config for `rafael`

--- nixos/rafael.nix
+++ nixos/rafael.nix
@@ -1,11 +1,23 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  # Enable the OpenSSH daemon
+  imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+    ];
+
+  # grub as bootloader
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    efiInstallAsRemovable = true; # needs this to work with my mobo 
+    device = "nodev";
+  };
+
   services.openssh = {
     enable = true;
-    permitRootLogin = "no";
-    passwordAuthentication = false;
+    settings.PermitRootLogin = "no";
+    settings.PasswordAuthentication = false;
   };
 
   # Enable Docker
@@ -19,22 +31,28 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "docker" ]; 
     openssh.authorizedKeys.keys = [
-      # persona mac
+      # personal mac
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKf35qWcRXme7A6qNvFXZE77gZR77VFkitxXlwPDQAzV drew.zemke@gmail.com" 
+      # work mac
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC91W695pYx41GogjRXOk5L4Fnmj76Uzb3jwWBhnV49G dnzemke@ucdavis.edu" 
     ];
   };
 
   # Basic system packages
   environment.systemPackages = with pkgs; [
+    docker-compose
     fish
-    helix
+    fzf
+    gcc
+    gh
+    git
+    gnumake
+    lazydocker
     openssl
     pkg-config
+    protobuf
     rustup
     vim
-    git
-    htop
-    docker-compose
   ];
 
   # Fish!
@@ -44,13 +62,89 @@
   # Set your time zone
   time.timeZone = "America/Los_Angeles";
 
-  # Firewall configuration
-  networking.hostName = "rafael";
-  networking.networkmanager.enable = true; 
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 22 ]; # SSH port
+  networking = {
+    hostName = "rafael";
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 22 53 80 443 ];
+      allowedUDPPorts = [ 53 ];
+
+    };
+    interfaces = {
+      eno1 = {
+        ipv4.addresses = [ {
+          address = "192.168.0.101"; 
+          prefixLength = 24;
+        } ];
+      };
+    };
+    defaultGateway = "192.168.0.1";
+    nameservers = [ "8.8.8.8" ];
+    extraHosts =
+    ''
+      127.0.0.1 pihole.rafael.local
+      127.0.0.1 hass.rafael.local
+    '';
   };
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
+
+  # FIXME
+  # services.ddclient = {
+  #   enable = true;
+  #   configFile = "/etc/ddclient.conf";
+  # };
+
+  # nginx
+  services.nginx = {
+    enable = true;
+    
+    # Recommended: Enable nginx status page
+    statusPage = true;
+
+    virtualHosts = {
+      "pihole.rafael.local" = {
+        locations."/" = {
+          proxyPass = "http://localhost:8080/admin";
+          proxyWebsockets = true;
+        };
+      };
+      "hass.rafael.local" = {
+        locations."/" = {
+          proxyPass = "http://localhost:8123";
+          proxyWebsockets = true;
+        };
+      };
+    };
+  };
+
+  # docker-compose
+  systemd.services.docker-compose = {
+    description = "Docker Compose Services";
+    requires = [ "docker.service" ];
+    after = [ "docker.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+      WorkingDirectory = "/home/drew/services";
+      ExecStart = "${pkgs.docker-compose}/bin/docker-compose up -d";
+      ExecStop = "${pkgs.docker-compose}/bin/docker-compose down";
+    };
+  };
+
+
 
   # Enable automatic system upgrades
   system.autoUpgrade.enable = true;
@@ -58,3 +152,4 @@
   # This value determines the NixOS release with which your system is to be compatible
   system.stateVersion = "24.05";
 }
+
