0f8800658f63b3fc2a863f2bde0e74e0c0f386f1
Drew Zemke <drew.zemke@gmail.com>
2024-11-22 22:56:13 -0800

    helix: improve 'map_spec_file' macro

--- fish/functions/map_spec_name.fish
+++ fish/functions/map_spec_name.fish
@@ -1,11 +1,17 @@
 #!/usr/bin/env fish
 #
-# if passed a .ts, .tsx, .js, or .jsx filenmae, creates a .spec.{ext} file and 
-
-# TODO: if there's already `.spec` in the name, remove it
-set regex 's/\.([^\.]*)$/.spec.\1/g'
+# if passed a .ts, .tsx, .js, or .jsx filenmae, 
+#  prints a .spec.{ext} filename
+# if pass a spec.{ext} filename, 
+#  prints the corresponding .{ext} filename
 
 function map_spec_name
     read input
+
+    set regex 's/\.spec\.([^\.]*)$/.\1/g'
+    if not string match -r '\.spec\.' $input > /dev/null
+        set regex 's/\.([^\.]*)$/.spec.\1/g'
+    end
+
     echo $input | sed -r $regex
 end
