1acfd01d15f29f26c0dcbb41f3ab01e79300f9f5
Drew Zemke <drew.zemke@gmail.com>
2025-03-11 11:05:54 -0700

    zellij: new tab script takes spaces into account for tab name

--- fish/scripts/zellij-new-tab.fish
+++ fish/scripts/zellij-new-tab.fish
@@ -8,7 +8,8 @@ set prompt (set_color blue)"Name"(set_color normal)": "(set_color yellow)
 read -P $prompt tab_name
 
 # see if z knows something
-set zoxide_result (zoxide query "$tab_name" 2> /dev/null)
+set tab_name_split (string split " " $tab_name)
+set zoxide_result (zoxide query $tab_name_split 2> /dev/null)
 
 # open a new tab
 if test -z $zoxide_result
