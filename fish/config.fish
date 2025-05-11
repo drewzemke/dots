fad1e20b512dac998843efdba9273882d4b0e005
Drew Zemke <drew.zemke@gmail.com>
2025-05-11 15:09:42 -0700

    fish: export openai api key in two ways

--- fish/config.fish
+++ fish/config.fish
@@ -38,6 +38,7 @@ end
 
 # put API keys into env 
 load_token OPENAI_API_TOKEN ~/.openai &
+load_token OPENAI_API_KEY ~/.openai &
 load_token ANTHROPIC_API_TOKEN ~/.anthropic &
 load_token ANTHROPIC_API_KEY ~/.anthropic &
 load_token OPENROUTER_API_KEY ~/.openrouter &
