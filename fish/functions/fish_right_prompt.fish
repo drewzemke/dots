3f9f0654063b440f8c568d1f74513e2246c04170
Drew Zemke <drew.zemke@gmail.com>
2025-07-12 12:34:58 -0700

    fish: fix jj/git prompts

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,3 +1,8 @@
 function fish_right_prompt 
+    set -l cmd_status $status
+    if test $cmd_status -ne 0
+        echo -n (set_color red)"✘ $cmd_status"(set_color normal)
+    end
+
     fish_vcs_prompt $argv
 end
