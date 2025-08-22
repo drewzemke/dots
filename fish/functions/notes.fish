ca5fd7bcf40ebb6c86c53836ddad2c7e20b20772
Drew Zemke <drew.zemke@gmail.com>
2025-08-22 08:31:27 -0700

    fish: improved `notes` script

--- fish/functions/notes.fish
+++ fish/functions/notes.fish
@@ -2,7 +2,17 @@
 #
 # open editor in the notes directory
 function notes
+    # go to notes dir
     cd ~/notes
-    $EDITOR .
+
+    # rename current tab if flag was passed
+    if test "$argv[1]" = "--rename"; or test "$argv[1]" = "-r"
+        zellij action rename-tab "notes"
+    end
+
+    # open editor to today's notes
+    $EDITOR ./daily/(date +%Y-%m-%d).md
+
+    # back to original dir
     cd -
 end
